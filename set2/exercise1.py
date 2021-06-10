656ee241adb50f853fd5ebaa9bce57db1b9f900a
HalleyFu <halley.fu@gmail.com>
2021-06-11 02:41:46 +1000

    commit

--- set2/exercise1.py
+++ set2/exercise1.py
@@ -11,8 +11,11 @@ import platform
 # I think this will print "hello! Let's get started" by calling the print function.
 print("hello! Let's get started")  # it printed "hello! Let's get started"
 
+# I think it will declare a variable called some_words
+# and it'll put a list of strings into it.
 some_words = ['what', 'does', 'this', 'line', 'do', '?']
 
+# I think it will print the string of words.
 for word in some_words:
     print(word)
 
